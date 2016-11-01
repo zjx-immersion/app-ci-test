477e4215f96c3910342e064acdcaa93d113a2ae7
zjx-immersion <jianxin_home@163.com>
2016-11-01 19:36:41 +0800

    update the returned valuse for testing build and deployment

--- app.go
+++ app.go
@@ -1,17 +1,20 @@
 package main
-import ( 
-           "fmt" 
-           "log" 
-           "net/http"
-           "github.com/gorilla/mux"
- )
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 func main() {
-          r := mux.NewRouter()
-          r.HandleFunc("/", Hello)
-          http.Handle("/", r)
-          fmt.Println("Starting up on 8080")
-          log.Fatal(http.ListenAndServe(":8080", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", Hello)
+	http.Handle("/", r)
+	fmt.Println("Starting up on 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 func Hello(w http.ResponseWriter, req *http.Request) {
-         fmt.Fprintln(w, "Hello world!")
+	fmt.Fprintln(w, "Hello world version 2!")
 }
