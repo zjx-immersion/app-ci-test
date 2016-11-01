181849091de72ca50bbd43442ee9d890522a48d2
zjx-immersion <jianxin_home@163.com>
2016-11-01 17:12:55 +0800

    added the rancher cli in code source for deploying later

---
+++ runEnv.sh
@@ -0,0 +1,6 @@
+# Set the url that Rancher is on
+export RANCHER_URL=http://52.197.115.91:8080/
+# Set the access key, i.e. username
+export RANCHER_ACCESS_KEY=EFBD4BE219FCE1C1261C
+# Set the secret key, i.e. password
+export RANCHER_SECRET_KEY=S3A5tAuRL4Eo2zswu5wuXijRzaPvcKDHL5Em3L54
